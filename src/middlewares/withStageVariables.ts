5ad42911d4cdffd8186f40db99f3c59006e2a7e7
Kittiphat S <freeuxer@gmail.com>
2021-01-13 13:34:28 +0700

    fix stage variable problem

--- src/middlewares/withStageVariables.ts
+++ src/middlewares/withStageVariables.ts
@@ -6,7 +6,7 @@ import { Logger } from '../utils/logger'
  * @param logger 
  */
 export const withStageVariables = (logger?: Logger): Middleware => async (ctx, next) => {
-  const stageVariables = ctx.req && ((ctx.req) as any).stageVariables
+  const stageVariables = ctx.req && ((ctx.req) as any).event?.stageVariables
   logger && logger.log(`[STG V] ${JSON.stringify(stageVariables)}`)
   ctx.stageVariables = stageVariables
   await next()
